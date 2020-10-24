538f9686dc58d6f85df731559fd88988937ffbc2
sapan <sapanostic@uf708313059bb51.ant.amazon.com>
2020-10-24 16:20:55 -0400

    Add actor plugin

---
+++ plugins/actor/build/CMakeFiles/actor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/actor.dir/actor.cc.o"
+  "libactor.pdb"
+  "libactor.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/actor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
