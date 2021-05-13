376ba384818ae557c1b20b7cac7385371f3b8bf5
sapan-ostic <sapanagrawal777@gmail.com>
2021-05-13 14:25:25 -0700

    major update: add plotting for F, min_obs_dis, rewards..

--- keras_rl/rl/agents/dqn_si.py
+++ keras_rl/rl/agents/dqn_si.py
@@ -338,6 +338,7 @@ class DQNAgentSi(DQNAgent):
         did_abort = False
         coes = []
         coes_for_plot = []
+        episode_min_obs_dis = []
 
         try:
             while not rospy.core.is_shutdown() and self.step < nb_steps:
@@ -434,6 +435,7 @@ class DQNAgentSi(DQNAgent):
                 metrics = self.backwardSip(reward, observation, done)
                 episode_reward += reward
 
+                coes = env.getState()
                 step_logs = {
                     'action': action,
                     'observation': observation,
@@ -441,11 +443,34 @@ class DQNAgentSi(DQNAgent):
                     'metrics': metrics,
                     'episode': episode,
                     'info': accumulated_info,
+                    'coes': coes
                 }
                 callbacks.on_step_end(episode_step, step_logs)
+                
+                if episode_step==0: 
+                    min_obs_dis_start = coes[5]
+                    episode_min_obs_dis.append(coes[5])
+                    plt.figure(88)
+                    plt.plot(episode_min_obs_dis)
+                    plt.xlabel('Episode')
+                    plt.ylabel('min_obs_dis_start')
+
                 episode_step += 1
                 self.step += 1
                 self.policy.change_tau(-0.0004)
+                env.setLoggingData(reward, coes, metrics[0], metrics[1], metrics[2], info['obs_dis'])
+
+                if logger is not None:
+                    logger.logOneFrame([self.step, coes[0], coes[1], coes[2],coes[3],coes[4],coes[5],
+                                reward, info['time'], info['obs_dis'],
+                                metrics[0], metrics[1], metrics[2]])
+
+                    # print('Step: {}\tAp: {:.2f}\tBp: {:.2f}\tL: {:.2f}\tReward: {:.2f}\tTime: {:.2f}\tDis: {:.2f}\tQ: {:.2f}'.format(
+                    #         self.step, coes[0], coes[1], coes[2], 
+                    #         reward, info['time'], info['obs_dis'],
+                    #         metrics[2]))
+
+                    logger.reOpen()
 
                 if verbose == 1 and self.step % log_interval == 0:
                     plt.figure(2)
@@ -457,17 +482,7 @@ class DQNAgentSi(DQNAgent):
                     print('coes: {}'.format(np.mean(coes)))
                     coes = []
 
-                if logger is not None:
-                    logger.logOneFrame([self.step, observation[0][2], observation[0][3],
-                                reward, info['time'], info['obs_dis'],
-                                metrics[0], metrics[1], metrics[2]])
 
-                    print('Step: {}\tA: {:.2f}\tD: {:.2f}\tReward: {:.2f}\tTime: {:.2f}\tDis: {:.2f}\tQ: {:.2f}'.format(
-                            self.step, observation[0][2], observation[0][3],
-                            reward, info['time'], info['obs_dis'],
-                            metrics[2]))
-
-                    logger.reOpen()
 
                 # if self.step % log_interval == 0:
                 #     result_str = ''
@@ -496,6 +511,7 @@ class DQNAgentSi(DQNAgent):
                         'nb_episode_steps': episode_step,
                         'nb_steps': self.step,
                         'coes': env.getState(),
+                        'min_obs_dis_start': min_obs_dis_start
                     }
                     callbacks.on_episode_end(episode, episode_logs)
                     if verbose == 1:
@@ -702,6 +718,9 @@ class DQNAgentSi(DQNAgent):
         self._on_test_begin()
         callbacks.on_train_begin()
         # test_init_state = 0.0
+        episode_min_obs_dis = []
+        episode_time = []
+        ER = []
         for episode in range(nb_episodes):
             callbacks.on_episode_begin(episode)
             episode_reward = 0.
@@ -740,6 +759,7 @@ class DQNAgentSi(DQNAgent):
             # Run the episode until we're done.
             done = False
             beyond_max_epi_steps = False
+            
             while not done and not beyond_max_epi_steps:
                 callbacks.on_step_begin(episode_step, self.model)
 
@@ -773,6 +793,7 @@ class DQNAgentSi(DQNAgent):
                 self.backwardSip(reward, observation, done)
                 episode_reward += reward
 
+
                 step_logs = {
                     'action': action,
                     'observation': observation,
@@ -781,9 +802,24 @@ class DQNAgentSi(DQNAgent):
                     'info': accumulated_info,
                 }
                 callbacks.on_step_end(episode_step, step_logs)
+
+                coes = env.getState()
+                # print("episode_step: ", episode_step)
+                if episode_step==0: 
+                    print("-------------------- PLOT -------------------")
+                    min_obs_dis_start = coes[5]
+                    episode_min_obs_dis.append(coes[5])
+                    plt.figure(88)
+                    plt.plot(episode_min_obs_dis, linewidth=2, color=[0, 0.4470, 0.7410])
+                    plt.xlabel('Episode')
+                    plt.ylabel('min obstacle distance (m)')
+
+
                 episode_step += 1
                 self.step += 1
 
+            episode_time.append(episode_step)
+            ER.append(episode_reward)
             # Report end of episode.
             episode_logs = {
                 'episode_reward': episode_reward,
@@ -794,6 +830,19 @@ class DQNAgentSi(DQNAgent):
             callbacks.on_episode_end(episode, episode_logs)
             print('test_init_state: {},\ttest_final_state: {}'.format(test_init_state, env.getState()))
             # test_init_state += 0.1
+        
+        # Plot time in terms of steps
+        plt.figure(99)
+        plt.title('Execution Time in no. of Steps')
+        plt.plot(episode_time, linewidth=2, color=[0, 0.4470, 0.7410])
+        plt.ylabel('No. of steps in Episode')
+        plt.xlabel('Episode')
+
+        plt.figure(1)
+        plt.title('Total Episode Reward')
+        plt.plot(ER, linewidth=2, color=[0, 0.4470, 0.7410])
+        plt.ylabel('Episodic Reward')
+        plt.xlabel('Episode')
 
         callbacks.on_train_end()
         self._on_test_end()
